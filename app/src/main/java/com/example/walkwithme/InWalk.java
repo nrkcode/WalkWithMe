54dd4f46b72b95a3d628b28061b38e5d2f3209b3
kshind <oikkl1114@naver.com>
2023-12-04 04:43:32 +0900

    산책 중 지도 출력

--- app/src/main/java/com/example/walkwithme/InWalk.java
+++ app/src/main/java/com/example/walkwithme/InWalk.java
@@ -1,6 +1,11 @@
 package com.example.walkwithme;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.Signature;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
@@ -14,6 +19,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import android.os.Build.VERSION;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -31,6 +38,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,12 +51,151 @@ import okhttp3.Response;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class InWalk extends AppCompatActivity {
+import net.daum.mf.map.api.CameraUpdateFactory;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapPointBounds;
+import net.daum.mf.map.api.MapPolyline;
+import net.daum.mf.map.api.MapView;
+
+public class InWalk extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapView.MapViewEventListener {
+    private MapView mapView;
+    private ViewGroup mapViewContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("키해시는 :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        // 권한ID를 가져옵니다
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET);
+
+        int permission2 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        int permission3 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        // 권한이 열려있는지 확인
+        if (permission == PackageManager.PERMISSION_DENIED || permission2 == PackageManager.PERMISSION_DENIED || permission3 == PackageManager.PERMISSION_DENIED) {
+            // 마쉬멜로우 이상버전부터 권한을 물어본다
+            if (VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // 권한 체크(READ_PHONE_STATE의 requestCode를 1000으로 세팅
+                requestPermissions(
+                        new String[]{Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        1000);
+            }
+            return;
+        }
+
+        //지도를 띄우자
+        // java code
+        mapView = new MapView(this);
+        mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+        mapView.setMapViewEventListener(this);
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
 
     }
+
+    // 권한 체크 이후로직
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+        // READ_PHONE_STATE의 권한 체크 결과를 불러온다
+        super.onRequestPermissionsResult(requestCode, permissions, grandResults);
+        if (requestCode == 1000) {
+            boolean check_result = true;
+
+            // 모든 퍼미션을 허용했는지 체크
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+
+            // 권한 체크에 동의를 하지 않으면 안드로이드 종료
+            if (check_result == false) {
+                finish();
+            }
+        }
+    }
+    // CurrentLocationEventListener 메소드 구현
+    @Override
+    public void onCurrentLocationUpdate(MapView mapView, MapPoint mapPoint, float v) {
+        // 현재 위치 업데이트 시 호출되는 메소드
+    }
+
+    @Override
+    public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
+        // 현재 위치 디바이스 헤딩 업데이트 시 호출되는 메소드
+    }
+
+    @Override
+    public void onCurrentLocationUpdateFailed(MapView mapView) {
+        // 현재 위치 업데이트 실패 시 호출되는 메소드
+    }
+
+    @Override
+    public void onCurrentLocationUpdateCancelled(MapView mapView) {
+        // 현재 위치 업데이트 취소 시 호출되는 메소드
+    }
+
+    // MapViewEventListener 메소드 구현
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+        // 지도 초기화 시 호출되는 메소드
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+        // 지도 중심 좌표 이동 시 호출되는 메소드
+    }
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+        // 지도 줌 레벨 변경 시 호출되는 메소드
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+        // 지도 싱글 탭 시 호출되는 메소드
+    }
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+        // 지도 싱글 탭 시 호출되는 메소드
+    }
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+        // 지도 싱글 탭 시 호출되는 메소드
+    }
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+        // 지도 싱글 탭 시 호출되는 메소드
+    }
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+        // 지도 싱글 탭 시 호출되는 메소드
+    }
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+        // 지도 싱글 탭 시 호출되는 메소드
+    }
+
 }
