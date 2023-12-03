3b9dc90f43d2c274d7fc58ce184e097d9d6a5c3f
kshind <oikkl1114@naver.com>
2023-12-04 03:08:36 +0900

    recyclerview추가
    recycler뷰 간단하게 추가해본 것 받아라 build.gradle 조심하기

--- app/src/main/java/com/example/walkwithme/HomeActivity.java
+++ app/src/main/java/com/example/walkwithme/HomeActivity.java
@@ -1,24 +1,82 @@
 package com.example.walkwithme;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class HomeActivity extends AppCompatActivity {
 
+    //아래부터recycler
+    private FusedLocationProviderClient fusedLocationClient;
+    private String kakaoApiKey = "bd2e505109d0952fa9d393afd54028aa";
+    private RecyclerView recyclerView;
+    private PlaceAdapter placeAdapter;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    //위부터recycler
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+
+        //아래부터recycler
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // 위치 권한 확인 및 요청
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE
+            );
+        } else {
+            requestLocation();
+        }
+
+        // RecyclerView 초기화
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        placeAdapter = new PlaceAdapter();
+        recyclerView.setAdapter(placeAdapter);
+        //위부터recycler
 
         // AsyncTask 실행
         new GetDataTask().execute();
@@ -57,7 +115,141 @@ public class HomeActivity extends AppCompatActivity {
             DataProcessor.processJsonData(result, nameTextView, lenTextView, hourTextView, caloryTextView);
         }
     }
+    private void requestLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    if (location != null) {
+                        // 위치 정보를 가져왔으면 API 호출
+                        searchNearbyPlaces(location.getLatitude(), location.getLongitude(), "공원");
+                    }
+                });
+    }
 
+    private void searchNearbyPlaces(double latitude, double longitude, String keyword) {
+        String url = "https://dapi.kakao.com/v2/local/search/keyword?y=" + latitude +
+                "&x=" + longitude +
+                "&radius=1000&query=" + keyword;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Authorization", "KakaoAK " + kakaoApiKey)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(@NonNull okhttp3.Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String responseData = response.body().string();
+                    runOnUiThread(() -> processResponse(responseData));
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull okhttp3.Call call, @NonNull IOException e) {
+                // Handle failure
+            }
+        });
+    }
+
+    private void processResponse(String responseData) {
+        try {
+            JSONObject jsonObject = new JSONObject(responseData);
+            JSONArray placesArray = jsonObject.getJSONArray("documents");
+
+            List<Place> places = new ArrayList<>();
+            for (int i = 0; i < placesArray.length(); i++) {
+                JSONObject placeObject = placesArray.getJSONObject(i);
+                String placeName = placeObject.getString("place_name");
+                String placeAddress = placeObject.getString("road_address_name");
+                String placeCategory = placeObject.getString("category_name");
+
+                places.add(new Place(placeName, placeAddress,placeCategory));
+            }
+
+            runOnUiThread(() -> placeAdapter.setPlaces(places));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Place {
+        private final String name;
+        private final String address;
+        private final String category;
+
+
+        public Place(String name, String address, String category) {
+            this.name = name;
+            this.address = address;
+            this.category = category;
+
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getAddress() {return address;}
+
+        public String getCategory() {
+            return category;
+        }
+
+    }
+
+    private static class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHolder> {
+        private List<Place> places = new ArrayList<>();
+
+        public void setPlaces(List<Place> places) {
+            this.places = places;
+            notifyDataSetChanged();
+        }
+
+        @NonNull
+        @Override
+        public PlaceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_list_item, parent, false);
+            return new PlaceViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PlaceViewHolder holder, int position) {
+            Place place = places.get(position);
+            holder.nameTextView.setText(place.getName());
+            holder.addressTextView.setText(place.getAddress());
+            holder.categoryTextView.setText(place.getCategory());
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return places.size();
+        }
+
+        public static class PlaceViewHolder extends RecyclerView.ViewHolder {
+            TextView nameTextView;
+            TextView addressTextView;
+            TextView categoryTextView;
+
+            public PlaceViewHolder(@NonNull View itemView) {
+                super(itemView);
+                nameTextView = itemView.findViewById(R.id.nameTextView);
+                addressTextView = itemView.findViewById(R.id.addressTextView);
+                categoryTextView = itemView.findViewById(R.id.categoryTextView);
+            }
+        }
+    }
 
 
 
--- app/src/main/java/com/example/walkwithme/InWalk.java
+++ app/src/main/java/com/example/walkwithme/InWalk.java
@@ -4,6 +4,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import androidx.appcompat.app.AppCompatActivity;
 
