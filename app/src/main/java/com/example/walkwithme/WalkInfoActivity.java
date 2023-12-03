6785984123039176d3ce21bfd1a4bdb32db1ae02
yeramm <213187@jnu.ac.kr>
2023-12-04 05:18:51 +0900

    산책 상세정보 화면
    대충 만들고 잡니다

---
+++ app/src/main/java/com/example/walkwithme/WalkInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.walkwithme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WalkInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walk_info);
+    }
+}
