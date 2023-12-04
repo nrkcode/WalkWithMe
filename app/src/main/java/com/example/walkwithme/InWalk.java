038392fd9fd3a772ba5ab187d5c0ecb84c3931f2
kshind <oikkl1114@naver.com>
2023-12-04 23:51:13 +0900

    inwalk.java 만보기 기능 추가

--- app/src/main/java/com/example/walkwithme/InWalk.java
+++ app/src/main/java/com/example/walkwithme/InWalk.java
@@ -65,13 +65,37 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
-public class InWalk extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapView.MapViewEventListener {
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class InWalk extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapView.MapViewEventListener, SensorEventListener {
     private MapView mapView;
     private ViewGroup mapViewContainer;
     private Chronometer chronometer; //타이머 view
     private boolean running; // 진행 중인지 확인하는 변수
     private long pauseOffset; // 정지 오프셋
+    SensorManager sensorManager;
+    Sensor stepCountSensor;
+    TextView stepCountView;
 
+    int currentSteps = 0;//현재 걸음수
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,6 +103,8 @@ public class InWalk extends AppCompatActivity implements MapView.CurrentLocation
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        stepCountView = findViewById(R.id.stepCountView);
 
         chronometer = findViewById(R.id.chronometer);
         chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener(){ // 시분초 표현
@@ -104,6 +130,23 @@ public class InWalk extends AppCompatActivity implements MapView.CurrentLocation
         ImageView walkImg = findViewById(R.id.walk_img);
 
 
+        // 활동 퍼미션 체크 (만보기쪽)
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+        }
+
+        // 걸음 센서 연결
+        // * 옵션
+        // - TYPE_STEP_DETECTOR:  리턴 값이 무조건 1, 앱이 종료되면 다시 0부터 시작
+        // - TYPE_STEP_COUNTER : 앱 종료와 관계없이 계속 기존의 값을 가지고 있다가 1씩 증가한 값을 리턴
+        //
+        // 디바이스에 걸음 센서의 존재 여부 체크
+        if (stepCountSensor == null) {
+            Toast.makeText(this, "No Step Sensor", Toast.LENGTH_SHORT).show();
+        }
+
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -128,6 +171,9 @@ public class InWalk extends AppCompatActivity implements MapView.CurrentLocation
                 stopBtn.setVisibility(stopBtn.VISIBLE);
                 walkImg.setVisibility(walkImg.VISIBLE);
                 resetBtn.setVisibility(resetBtn.VISIBLE);
+
+                sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+                stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
 
                 if(!running){
                     chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
@@ -286,6 +332,43 @@ public class InWalk extends AppCompatActivity implements MapView.CurrentLocation
     @Override
     public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
         // 지도 싱글 탭 시 호출되는 메소드
+    }
+
+    public void onStart() {
+        super.onStart();
+        if(stepCountSensor !=null) {
+            // 센서 속도 설정
+            // * 옵션
+            // - SENSOR_DELAY_NORMAL: 20,000 초 딜레이
+            // - SENSOR_DELAY_UI: 6,000 초 딜레이
+            // - SENSOR_DELAY_GAME: 20,000 초 딜레이
+            // - SENSOR_DELAY_FASTEST: 딜레이 없음
+            //
+            sensorManager.registerListener(this,stepCountSensor,SensorManager.SENSOR_DELAY_FASTEST);
+        }
+    }
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // 걸음 센서 이벤트 발생시
+        if(event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR){
+
+            if(event.values[0]==1.0f){
+                // 센서 이벤트가 발생할때 마다 걸음수 증가
+                currentSteps++;
+                stepCountView.setText(String.valueOf(currentSteps));
+            }
+
+        }
+
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
 }
