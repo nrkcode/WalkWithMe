1449f488430ae0fb13aa1e9b5f1e28d9f6cb9c71
park hyun <104882490+parkhyun55@users.noreply.github.com>
2023-12-04 21:10:44 +0900

    뒤로가기 오류 해결

--- app/src/main/java/com/example/walkwithme/Detail.java
+++ app/src/main/java/com/example/walkwithme/Detail.java
@@ -64,7 +64,7 @@ public class Detail extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Detail.class);
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(intent);
             }
         });
