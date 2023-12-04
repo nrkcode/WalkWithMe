e535462d6d4dd008c6939499ce5396da31b2f878
park hyun <104882490+parkhyun55@users.noreply.github.com>
2023-12-05 03:38:38 +0900

    별로 한건 없지만 업데이트용 push
    ㄱ ㅣ ㄹ  ㄷ ㅏ
    ㅗ ㅣ ㅗ
            ㅂ

--- app/src/main/java/com/example/walkwithme/WalkInfoActivity.java
+++ app/src/main/java/com/example/walkwithme/WalkInfoActivity.java
@@ -1,12 +1,39 @@
 package com.example.walkwithme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class WalkInfoActivity extends AppCompatActivity {
 
@@ -28,6 +55,17 @@ public class WalkInfoActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //산책 시작 버튼 클릭시 타이머화면으로 인텐트
+        Button button2 = (Button) findViewById(R.id.start_walk_btn);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), InWalk.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
